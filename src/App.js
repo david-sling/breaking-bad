724ee3d1d5eb143b919852218cd27b434fd98d2a
David S D <sddavid.johan@gmail.com>
2021-08-09 14:56:15 +0530

    Added davidsling.in link to console

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Character from "./pages/Character";
 import Home from "./pages/Home";
 
 export default function App() {
+  useEffect(() => {
+    console.log("Hey there, do checkout https://davidsling.in");
+  }, []);
   return (
     <div className="App">
       <Router>
