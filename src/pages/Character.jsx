8f2e32b8b24920c11dde487efdd826a4ab555064
David S D <sddavid.johan@gmail.com>
2021-08-09 15:07:28 +0530

    Added dynamic title

--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import useCharacter from "../hooks/useCharacter";
 import CharacterPreview from "../components/CharacterPreview";
+import { Helmet } from "react-helmet";
 
 export default function Character() {
   const { id } = useParams();
   const { character } = useCharacter(id);
   const { appearance, portrayed } = character;
   return (
-    <div className="Character">
-      <CharacterPreview {...character} full />
-      {/* <div className="Details"></div> */}
-    </div>
+    <>
+      <Helmet>
+        <title>{`${character.name || ""} | BREAKING BAD`}</title>
+      </Helmet>
+      <div className="Character">
+        <CharacterPreview {...character} full />
+        {/* <div className="Details"></div> */}
+      </div>
+    </>
   );
 }
